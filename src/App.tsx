0eaf8a80446600091e7e866b2f1332e5680aff7a
Beena Evangelin <graciaevangelin99@gmail.com>
2023-09-18 13:16:45 +0530

    Added global state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { useState } from "react";
 import Todos from "./components/Todos";
-import Todo from "./Data/Todo";
 import NewTodo from "./components/NewTodo";
 import Header from "./components/Header";
+import TodosContextProvider from "./store/TodoContext";
 
 const App = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-    setTodos((prevTodo) => {
-      return prevTodo.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodo) => {
-      return prevTodo.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <div>
+    <TodosContextProvider>
       <Header />
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 };
 
